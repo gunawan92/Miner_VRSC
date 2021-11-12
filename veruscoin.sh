29938bd32c083a1f66aa62bcd903a8280f589369
gunawanwp <gunwp2@gmail.com>
2021-11-12 11:52:46 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,4 +1,4 @@
-sudo apt update
+sudo apt-get install update
 https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
+tar -vxf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RBg3Lz5LYThMYDtMZ2MZmTZXB5xw6u7WK9.Hell01 -p x --cpu 4 
